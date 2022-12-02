a62a360ac60a136c3d2a08a277c476ef60811644
mateus-werneck <werneck.mateus@gmail.com>
2022-12-02 15:22:10 -0300

    Ajustes

--- SchedulerV2/App/Lib/Errors/Auth/user_not_allowed_exception.py
+++
@@ -1,4 +0,0 @@
-class UserNotAllowedException(Exception):
-    def __init__(self, user_id: int):
-        self.message = f'User {user_id} not allowed to use bot.'
-        super().__init__(self.message)
--- SchedulerV2/App/Lib/Treat/list_treat.py
+++
@@ -1,2 +0,0 @@
-def list_split(value: list, parts: int):
-    return [value[i:i+parts] for i in range(0, len(value), parts)]
