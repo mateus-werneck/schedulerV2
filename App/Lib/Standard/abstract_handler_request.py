6475c86d9a0b077ae3609258080a177d5a247604
mateus-werneck <werneck.mateus@gmail.com>
2022-12-03 10:24:56 -0300

    Ajustes

--- App/Lib/Standard/abstract_handler_request.py
+++ App/Lib/Standard/abstract_handler_request.py
@@ -49,12 +49,16 @@ class AbstractHandlerRequest(ABC):
     def get_handlers(self):
         return list(self.__instances)
 
-    def remove_handler(self, handler: str):
+    def reset_handler(self, handler: str):
+        if not self.has_handler(handler):
+            return
         self.__instances[handler].step = None
-        del self.__instances[handler]
-        message = f'[*] Removing handler: {handler}'
+        message = f'[*] Reseting handler: {handler}'
         Logger.instance().info(message, context=self)
 
+    def has_handler(self, handler:str):
+        return handler in self.get_handlers()
+    
     def execute(self, update: Update = None, context: CallbackContext = None):
         if not self.__handle_update(update, context):
             return
@@ -139,7 +143,7 @@ class AbstractHandlerRequest(ABC):
 
         BotMode.instance().clear_mode()
         handler = self.get_handler_name()
-        self.remove_handler(handler)
+        self.reset_handler(handler)
 
     def should_finish(self, force: bool):
         if force:
