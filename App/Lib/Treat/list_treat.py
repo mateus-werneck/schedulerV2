7054a37e2c488bda2ff73e8ab4e4552e0665e63f
mateus-werneck <werneck.mateus@gmail.com>
2022-12-06 19:36:16 -0300

    Ajustes

--- App/Lib/Treat/list_treat.py
+++ App/Lib/Treat/list_treat.py
@@ -1,4 +1,2 @@
 def list_split(value: list, parts: int):
-    if not parts:
-        return value
     return [value[i:i+parts] for i in range(0, len(value), parts)]
--- App/Queues/Schedule/Listing/Grade/Steps/list_categories.py
+++ App/Queues/Schedule/Listing/Grade/Steps/list_categories.py
@@ -32,7 +32,7 @@ class ListCategories(ListGradeOptions):
     def get_menu(self):
         options = self.get_options()
         append_exit_button(options)
-        return treat_menu(options, 'agenda_grades', 0)
+        return treat_menu(options, 'agenda_grades', 2)
 
     def get_options(self):
         return [
