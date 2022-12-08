0a6d980553a2ca333dbb8fd41f78ee3d0869be0a
mateus-werneck <werneck.mateus@gmail.com>
2022-12-08 15:26:53 -0300

    Ajustes

--- App/Queues/Standard/factory_queue.py
+++ App/Queues/Standard/factory_queue.py
@@ -5,17 +5,17 @@ from App.Lib.Standard.abstract_singleton import AbstractSingleton
 
 class FactoryQueue(AbstractSingleton):
     _queues = {}
-    
+
     @staticmethod
     def create(namespace: str):
         module = FactoryQueue.get_module(namespace)
         class_name = FactoryQueue.get_class_name(namespace)
         queue_class = getattr(module, class_name)
-        
+
         FactoryQueue.delete_queue(class_name)
         FactoryQueue._queues[class_name] = queue_class()
         return FactoryQueue._queues[class_name]
-    
+
     @staticmethod
     def get_module(namespace: str):
         module_namespace = f'{FactoryQueue.get_base_namespace()}.{namespace}'
@@ -24,15 +24,14 @@ class FactoryQueue(AbstractSingleton):
     @staticmethod
     def get_base_namespace():
         return 'App.Queues'
-    
+
     @staticmethod
     def get_class_name(namespace: str):
         module_name = namespace.split('.').pop()
         name_parts = module_name.split('_')
         return ''.join([name.capitalize() for name in name_parts])
-    
+
     @staticmethod
-    def delete_queue(class_name:str):
+    def delete_queue(class_name: str):
         if FactoryQueue._queues.get(class_name):
-            del FactoryQueue._queues[class_names]
-    
+            del FactoryQueue._queues[class_name]
