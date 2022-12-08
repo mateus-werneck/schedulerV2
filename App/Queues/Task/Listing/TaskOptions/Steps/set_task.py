bbb821ce56cffa15fb364d129052bb6b3f18e652
mateus-werneck <werneck.mateus@gmail.com>
2022-12-08 10:11:05 -0300

    Ajustes

--- App/Queues/Task/Listing/TaskOptions/Steps/set_task.py
+++ App/Queues/Task/Listing/TaskOptions/Steps/set_task.py
@@ -1,12 +1,12 @@
 from App.Lib.Bot.chat import BotChat
-from App.Queues.Task.Listing.TaskOptions.list_task_options \
-    import ListTaskOptions
+from App.Queues.Task.Listing.TaskOptions.list_task_options import \
+    ListTaskOptions
 
 
 class SetTask(ListTaskOptions):
 
     def handle(self) -> bool:
         task = BotChat.instance().extract_callback_data()
-        task = task.replace('main_tasks', '')
+        task = task.replace('main_tasks_', '')
         self.set_task(task)
         return super().handle()
