02cb8eebf41adb1d250764f0fb22941b28bab83f
mateus-werneck <werneck.mateus@gmail.com>
2022-12-08 11:33:40 -0300

    Ajustes

--- App/Queues/Jobs/Daily/Steps/set_jobs.py
+++ App/Queues/Jobs/Daily/Steps/set_jobs.py
@@ -1,7 +1,8 @@
-from App.Queues.Jobs.Daily.update import Update
-from App.Data.Helpers.job_queue_helper \
-    import append_today_tasks, delete_expired_schedules
+from App.Data.Helpers.job_queue_helper import (append_today_tasks,
+                                               delete_expired_schedules)
 from App.Lib.Treat.time_treat import treat_string_hour_to_time
+from App.Queues.Jobs.Daily.update import Update
+
 
 class SetJobs(Update):
 
@@ -15,13 +16,13 @@ class SetJobs(Update):
     def append_delete_expired_schedule(self, jobs: list):
         jobs.append({
             'callback': delete_expired_schedules,
-            'time': treat_string_hour_to_time('03:00'),
+            'time': treat_string_hour_to_time('02:00'),
             'name': 'Delete Expired Schedules'
         })
 
     def append_add_tasks_job(self, jobs: list):
         jobs.append({
             'callback': append_today_tasks,
-            'time': treat_string_hour_to_time('03:01'),
+            'time': treat_string_hour_to_time('02:01'),
             'name': 'Add Today Tasks'
         })
