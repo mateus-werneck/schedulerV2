17522841e69ea7212a5236c65e8be3dd1c17caef
mateus-werneck <werneck.mateus@gmail.com>
2022-12-09 18:57:37 -0300

    Ajustes

--- App/Queues/Schedule/Listing/Daily/Steps/find_tasks.py
+++ App/Queues/Schedule/Listing/Daily/Steps/find_tasks.py
@@ -2,6 +2,7 @@ from App.Queues.Schedule.Listing.Daily.list_daily import ListDaily
 from App.Lib.Client.marina_api import MarinaAPI
 from App.Data.Helpers.task_helper import get_tasks_from_schedules
 from App.Lib.Treat.time_treat import treat_datetime_to_string_hour
+from App.Lib.Treat.date_treat import treat_node_string
 
 
 class FindTasks(ListDaily):
@@ -14,10 +15,12 @@ class FindTasks(ListDaily):
     def find_daily(self):
         daily = MarinaAPI.instance().list_today_tasks()
         tasks = get_tasks_from_schedules(daily)
-        return [treat_task(task) for task in tasks]
+        return [self.treat_task(task) for task in tasks]
 
-    def treat_task(task: dict):
-        deadline = treat_datetime_to_string_hour(task.get('deadLine'))
+    def treat_task(self, task: dict):
+        deadline = treat_node_string(task.get('deadLine'))
+        deadline = treat_datetime_to_string_hour(deadline)
+        
         return {
             'id': task.get('id'),
             'name': f'{task.get("name")} - {deadline}'
